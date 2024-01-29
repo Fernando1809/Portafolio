3cbbe016b28566cbaa9c85738d6c4a23300f9f52
Fernando1809 <fernafdre1718.frg@gmail.com>
2024-01-29 15:05:47 -0600

    20224.01.29

---
+++ index.js
@@ -0,0 +1,64 @@
+// Función para verificar la posición y mostrar/ocultar el botón de inicio y actualizar la edad
+function checkScrollPosition() {
+    var aboutSection = document.getElementById('biografia');
+    var btnHome = document.getElementById('btnHome');
+    var edadSpan = document.querySelector('.EdadFG');
+  
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+  
+    // Muestra u oculta el botón de inicio
+    btnHome.style.display = scrollPosition >= aboutSection.offsetTop ? 'block' : 'none';
+  
+    // Calcula la edad y actualiza el span correspondiente
+    var fechaNacimiento = new Date('2001-09-18');
+    var hoy = new Date();
+    var edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+  
+    // Ajusta la edad si aún no ha pasado el cumpleaños
+    if (hoy.getMonth() < fechaNacimiento.getMonth() || (hoy.getMonth() === fechaNacimiento.getMonth() && hoy.getDate() < fechaNacimiento.getDate())) {
+      edad--;
+    }
+  
+    // Actualiza el contenido del span con la nueva edad
+    edadSpan.textContent = edad;
+  }
+  
+  // Función para llevar al usuario al inicio
+  function scrollToTop() {
+    document.body.scrollTop = 0; // Para navegadores antiguos
+    document.documentElement.scrollTop = 0; // Para navegadores modernos
+  }
+  
+  // Evento de desplazamiento para verificar la posición y mostrar/ocultar el botón de inicio y actualizar la edad
+  window.addEventListener('scroll', checkScrollPosition);
+  
+  // Llama a la función inicialmente para configurar la edad en la carga de la página
+  checkScrollPosition();
+
+//MODO OSCURO
+document.addEventListener('DOMContentLoaded', function() {
+  // Verificar el modo actual almacenado en localStorage o establecer el modo claro por defecto
+  var darkMode = localStorage.getItem('darkMode');
+  var body = document.getElementById('bodyContainer');
+  var darkModeSwitch = document.getElementById('darkModeSwitch');
+  var modeIcon = document.getElementById('modeIcon');
+
+  // Función para alternar entre los modos
+  function toggleDarkMode() {
+    body.classList.toggle('dark-mode-container');
+    darkModeSwitch.classList.toggle('dark-mode-switch');
+    // Cambiar el ícono según el modo actual
+    modeIcon.textContent = body.classList.contains('dark-mode-container') ? '🌙' : '☀️';
+    // Almacenar el modo actual en localStorage
+    localStorage.setItem('darkMode', body.classList.contains('dark-mode-container') ? 'enabled' : 'disabled');
+  }
+
+  // Establecer el modo claro por defecto si no hay una preferencia almacenada
+  if (darkMode === 'enabled') {
+    toggleDarkMode(); // Si estaba en modo oscuro, cambiar a claro al cargar la página
+  }
+
+  // Asignar el evento de clic al interruptor de modo oscuro
+  darkModeSwitch.addEventListener('click', toggleDarkMode);
+});
+
